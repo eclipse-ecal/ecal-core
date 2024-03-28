b6be5270d6826dd3f953453d1639f0cf0067dfd3
Kerstin Keller <KerstinKeller@users.noreply.github.com>
2024-03-28 15:36:48 +0100

    [build] Handle dependencies with a dependency provider script IF we want to use the dependency providers, the minimum required CMake Version is 3.24.

--- build/linux/01_build_default.sh
+++ build/linux/01_build_default.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/../.."
 mkdir -p _01_build_default
 cd _01_build_default
 
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES="submodule_dependencies.cmake"
 make -j
 
 # Run tests if available
