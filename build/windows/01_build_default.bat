530b7d44d90c46774b74588c035833431f49d8ee
KerstinKeller <KerstinKeller@users.noreply.github.com>
2024-03-28 17:14:44 +0100

    [build] Handle dependencies with a dependency provider script (#52)
    IF we want to use the dependency providers, the minimum required CMake Version is 3.24.

--- build/windows/01_build_default.bat
+++ build/windows/01_build_default.bat
@@ -5,7 +5,7 @@ pushd "%~dp0\..\.."
 mkdir _01_build_default
 cd _01_build_default
 
-cmake .. -A x64
+cmake .. -A x64 -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES="submodule_dependencies.cmake"
 cmake --build . --parallel --config Release
 ctest -C Release -V
 
--- thirdparty/ecaludp/Modules/Findecaludp.cmake
+++
@@ -1,1 +0,0 @@
-set(ecaludp_FOUND TRUE)
